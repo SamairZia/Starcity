15593b2f542e4d7a298aa8cdc10a25ba90d7bd1d
Samair Zia <samairzia1995@gmail.com>
2018-12-31 02:15:20 +0500

    Repo created

---
+++ app/src/main/java/com/example/samair/starcity/MallMap.java
@@ -0,0 +1,13 @@
+package com.example.samair.starcity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MallMap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mall_map);
+    }
+}
