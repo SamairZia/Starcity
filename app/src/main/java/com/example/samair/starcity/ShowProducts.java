84a3f02c832dd334a982e3cc0f995330db32954c
Samair Zia <samairzia1995@gmail.com>
2019-01-02 19:38:30 +0500

    Add products done

---
+++ app/src/main/java/com/example/samair/starcity/ShowProducts.java
@@ -0,0 +1,32 @@
+package com.example.samair.starcity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ShowProducts extends AppCompatActivity {
+
+    private Button btn_openAddproduct;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_products);
+
+        btn_openAddproduct = findViewById(R.id.btn_openAddproduct);
+
+        btn_openAddproduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addProduct();
+            }
+        });
+
+    }
+     public void addProduct(){
+         Intent intentAddProduct = new Intent(this, AddProducts.class);
+         startActivity(intentAddProduct);
+     }
+}
